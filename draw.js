49023fb3eb53a32232348b69f0252e240b8e223a
orionkumikuze <69163151+orionkumikuze@users.noreply.github.com>
2022-10-22 19:22:05 +0200

    Update draw.js

--- draw.js
+++ draw.js
@@ -20,3 +20,10 @@ function drawSprite(x,y,image) {
 function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+keys = {
+    q: 81, w: 87, e: 69, r: 82, t: 84, y: 89, u: 85, i: 73, o: 79, p: 80,
+    a: 65, s: 83, d: 68, f: 70, g: 71, h: 72, j: 74, k: 75, l: 76,
+    z: 90, x: 88, c: 67, v: 86, b: 66, n: 78, m: 77,
+    space: 32,
+    one: 49, two: 50, three: 51, four: 52, five: 53, six: 54, seven: 55, eight: 56, nine: 57, zero: 48
+}
